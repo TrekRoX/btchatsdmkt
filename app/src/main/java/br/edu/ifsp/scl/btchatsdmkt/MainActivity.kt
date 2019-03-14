64d35a88b2fed0ced70c81ce90c28cc5d9ab7c5e
Willian de Lima <will_trk@hotmail.com>
2019-03-13 21:20:57 -0300

    Solução Tarefa 1

--- app/src/main/java/br/edu/ifsp/scl/btchatsdmkt/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/btchatsdmkt/MainActivity.kt
@@ -17,10 +17,12 @@ import android.os.Handler
 import android.os.Message
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.PermissionChecker
+import android.view.LayoutInflater
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.RadioButton
 import android.widget.Toast
 import br.edu.ifsp.scl.btchatsdmkt.BluetoothSingleton.Constantes.ATIVA_BLUETOOTH
 import br.edu.ifsp.scl.btchatsdmkt.BluetoothSingleton.Constantes.ATIVA_DESCOBERTA_BLUETOOTH
@@ -31,6 +33,7 @@ import br.edu.ifsp.scl.btchatsdmkt.BluetoothSingleton.Constantes.TEMPO_DESCOBERT
 import br.edu.ifsp.scl.btchatsdmkt.BluetoothSingleton.adaptadorBt
 import br.edu.ifsp.scl.btchatsdmkt.BluetoothSingleton.outputStream
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_activity.*
 import java.io.IOException
 
 class  MainActivity : AppCompatActivity() {
@@ -50,6 +53,9 @@ class  MainActivity : AppCompatActivity() {
 
     // Handler da tela principal
     var mHandler: TelaPrincipalHandler? = null
+
+    //Indica modo de execucao
+    var modoServidor: Boolean? = null
 
     // Dialog para aguardar conexões e busca
     private var aguardeDialog: ProgressDialog? = null
@@ -71,6 +77,55 @@ class  MainActivity : AppCompatActivity() {
                 (ActivityCompat.checkSelfPermission(this,Manifest.permission.ACCESS_COARSE_LOCATION) != PermissionChecker.PERMISSION_GRANTED)) {
                    ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION),REQUER_PERMISSOES_LOCALIZACAO)
                }
+        }
+
+        showDialog()
+    }
+
+    fun showDialog(){
+        //Inflando layout do dialog
+        val optDialogView = LayoutInflater.from(this).inflate(R.layout.dialog_activity, null)
+
+        //Builder do dialog com as opções
+        val optBuilder = AlertDialog.Builder(this).setView(optDialogView).setTitle("Modo de operação:")
+
+        //Show do dialog
+        val optDialog = optBuilder.show()
+
+        //Eventos radio button dialog
+        optDialog.radioServidor.setOnClickListener{ onRadioButtonOptClicked(view = it) }
+        optDialog.radioCliente.setOnClickListener{ onRadioButtonOptClicked(view = it) }
+
+        //Eventos do dialog
+        optDialog.btnSalvarConf.setOnClickListener{
+            if(modoServidor != null)
+            {
+                optDialog.dismiss()
+                inicializa()
+            }
+            else
+                toast( "Selecione um modo de operação")
+        }
+    }
+
+    fun inicializa()
+    {
+        if(modoServidor!!)
+            setaModoServidor()
+        else
+            setaModoCliente()
+    }
+
+    fun onRadioButtonOptClicked(view: View) {
+
+        if (view is RadioButton) {
+            // Check which radio button was clicked
+            when (view.getId()) {
+
+                R.id.radioServidor ->  modoServidor = view.isChecked
+
+                R.id.radioCliente -> modoServidor = !view.isChecked
+            }
         }
     }
 
@@ -128,28 +183,35 @@ class  MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
         var retorno = false
         when (item?.itemId) {
-            R.id.modoClienteMenuItem -> {
-                toast("Configurando modo cliente")
-                // (Re)Inicializando a Lista de dispositivos encontrados
-                listaBtsEncontrados = mutableListOf()
-
-                registraReceiver()
-
-                adaptadorBt?.startDiscovery()
-
-                exibirAguardeDialog("Procurando dispositivos Bluetooth", 0)
+ /*           R.id.modoClienteMenuItem -> {
+                setaModoCliente()
                 retorno = true
             }
             R.id.modoServidorMenuItem -> {
-                toast("Configurando modo servidor")
-
-                val descobertaIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
-                descobertaIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,TEMPO_DESCOBERTA_SERVICO_BLUETOOTH)
-                startActivityForResult(descobertaIntent,ATIVA_DESCOBERTA_BLUETOOTH)
+                setaModoServidor()
+                retorno = true
+            }*/
+            R.id.refreshMenuItem ->  {
+                inicializa()
                 retorno = true
             }
         }
         return retorno
+    }
+    fun setaModoCliente(){
+                // (Re)Inicializando a Lista de dispositivos encontrados
+        listaBtsEncontrados = mutableListOf()
+
+        registraReceiver()
+
+        adaptadorBt?.startDiscovery()
+
+        exibirAguardeDialog("Procurando dispositivos Bluetooth", 0)
+    }
+    fun setaModoServidor()  {
+        val descobertaIntent = Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE)
+        descobertaIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION,TEMPO_DESCOBERTA_SERVICO_BLUETOOTH)
+        startActivityForResult(descobertaIntent,ATIVA_DESCOBERTA_BLUETOOTH)
     }
 
     private fun registraReceiver() {
@@ -224,17 +286,30 @@ class  MainActivity : AppCompatActivity() {
     fun exibirDispositivosEncontrados(){
         aguardeDialog?.dismiss()
 
-        val listaNomeBtsEncontrados: MutableList<String> = mutableListOf()
-        listaBtsEncontrados?.forEach{ listaNomeBtsEncontrados.add(if(it.name == null) "Sem nome" else it.name)}
+        if(listaBtsEncontrados?.size!! > 0)
+        {
+            val listaNomeBtsEncontrados: MutableList<String> = mutableListOf()
+            listaBtsEncontrados?.forEach{ listaNomeBtsEncontrados.add(if(it.name == null) "Sem nome" else it.name)}
 
-        val escolhaDispositivoDialog = with(AlertDialog.Builder(this)){
-            setTitle("Dispositivos encontrados")
-            setSingleChoiceItems(listaNomeBtsEncontrados.toTypedArray(),
-                -1
-            ) { dialog, which ->  trataSelecaoServidor(dialog, which)}
+            val escolhaDispositivoDialog = with(AlertDialog.Builder(this)){
+                setTitle("Dispositivos encontrados")
+                setSingleChoiceItems(listaNomeBtsEncontrados.toTypedArray(),
+                    -1
+                ) { dialog, which ->  trataSelecaoServidor(dialog, which)}
+            }
+
+            escolhaDispositivoDialog.show()
         }
+        else
+        {
+            val escolhaDispositivoDialog = with(AlertDialog.Builder(this)){
+                setTitle("Dispositivos encontrados")
+                setMessage("Nenhum dispositivo encontrado")
 
-        escolhaDispositivoDialog.show()
+            }
+
+            escolhaDispositivoDialog.show()
+        }
     }
 
     private fun trataSelecaoServidor(dialog: DialogInterface, which: Int){
